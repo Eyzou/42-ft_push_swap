f7475091b8025bc7ce70343377b20cfe310a2ec0
Elodie Hamm <elo@MBP-de-Elodie.fritz.box>
2024-04-11 18:00:34 +0200

    hi

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ehamm <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: elo <elo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 15:11:58 by ehamm             #+#    #+#             */
-/*   Updated: 2024/04/11 16:23:33 by ehamm            ###   ########.fr       */
+/*   Updated: 2024/04/11 18:00:19 by elo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int			space(int c);
 void		ft_error(void);
 void		ft_free(t_stack **lst);
 t_stack		*ft_lstlast(t_stack *lst);
+
 void		ft_ra(t_stack **a, int j);
 void		ft_rb(t_stack **b, int j);
 void		ft_sa(t_stack **a, int j);
@@ -53,7 +54,7 @@ void		ft_ss(t_stack **a, t_stack **b, int j);
 void		ft_rr(t_stack **a, t_stack **b, int j);
 void		ft_rrr_sub(t_stack **b, int j);
 void		ft_rrr(t_stack **a, t_stack **b, int j);
-t_stack		*ft_lstlast(t_stack *lst);
+
 int			ft_lstsize(t_stack *lst);
 int			ft_min(t_stack *a);
 int			ft_max(t_stack *a);
@@ -82,14 +83,11 @@ int			ft_apply_rrarrb(t_stack **a, t_stack **b, int c, char s);
 int			ft_apply_rrarb(t_stack **a, t_stack **b, int c, char s);
 int			ft_apply_rarrb(t_stack **a, t_stack **b, int c, char s);
 void		ft_rrb(t_stack **b, int j);
-void		ft_check_sub(t_stack **a, t_stack **b, char *line);
-char		*ft_check(t_stack **a, t_stack **b, char *line);
-void		ft_checker_sub(t_stack **a, t_stack **b, char *line);
+
 t_stack		*ft_process(int argc, char **argv);
 void		ft_sb(t_stack **b, int j);
-void		algorithm(int argc, char **argv);
 void		ft_sort_b_till_3(t_stack **stack_a, t_stack **stack_b);
 t_stack		*ft_sub_process(char **argv);
-void		ft_error_ch(void);
+
 
 #endif
--- srcs/args_process.c
+++ srcs/args_process.c
@@ -6,46 +6,11 @@
 /*   By: elo <elo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 16:33:25 by ehamm             #+#    #+#             */
-/*   Updated: 2024/04/11 17:38:42 by elo              ###   ########.fr       */
+/*   Updated: 2024/04/11 17:54:44 by elo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
-// This function does three things.
-// 1. It checks if the number of input is less than 2.
-// 2. It checks if the number of input is equal to 2.
-//    If it is, it means it is a quoted string.
-// 3. It checks if the number of input is greater than 2.
-//     If it is, it lists the arguements.
-int	ft_atoi2(const char *str)
-{
-	int				mod;
-	long long int	i;
-
-	i = 0;
-	mod = 1;
-	while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\f'
-		|| *str == '\v' || *str == '\r')
-		str++;
-	if (*str == '-')
-	{
-		mod = -1;
-		str++;
-	}
-	else if (*str == '+')
-		str++;
-	while (*str)
-	{
-		if (!ft_isdigit(*str))
-			ft_error();
-		i = i * 10 + (*str - 48);
-		str++;
-	}
-	if ((mod * i) > 2147483647 || (mod * i) < -2147483648)
-		ft_error();
-	return (mod * i);
-}
 
 // This function works and sorts the stacks
 // in case of they are passed in between quotation
@@ -64,7 +29,7 @@ t_stack	*ft_sub_process(char **argv)
 	tmp = ft_split(argv[1], 32);
 	while (tmp[i])
 	{
-		j = ft_atoi2(tmp[i]);
+		j = ft_atoi(tmp[i]);
 		ft_add_back(&a, ft_stack_new(j));
 		i++;
 	}
@@ -96,7 +61,7 @@ t_stack	*ft_process(int argc, char **argv)
 	{
 		while (i < argc)
 		{
-			j = ft_atoi2(argv[i]);
+			j = ft_atoi(argv[i]);
 			ft_add_back(&a, ft_stack_new(j));
 			i++;
 		}
